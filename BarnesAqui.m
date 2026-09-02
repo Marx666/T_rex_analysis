ee2a4ae30f317a8d358520133d6b1838930e4e55
MoHeart666 <moheart666@outlook.com>
2026-09-02 15:19:16 +0800

    BarnesAqui.m

--- BarnesAqui.m
+++ BarnesAqui.m
@@ -1,5 +1,7 @@
-pilo = [1 2 3 4 5 6 21 22 23 24 25 26 27 28 29 30 37 38 39 40 41 42 43 44 45 46];
-ctrl = [7 8 9 10 11 12 13 14 15 16 17 18 19 20 31 32 33 34 35 36];
+%%Pool velocity data from all Pilo/Ctrl animals together.
+
+pilo = [1 2 3 4 5 6 21 22 23 24 25 26 27 28 29 30 37 38 39 40 41 42 43 44 45 46]; % Pilo animal number
+ctrl = [7 8 9 10 11 12 13 14 15 16 17 18 19 20 31 32 33 34 35 36]; % Ctrl animal number
 
 sample_width = 30;  %% 1s
 overlap = 10;
@@ -17,4 +19,4 @@ end
 mean_v_pilo = [mean_v{pilo}];
 mean_v_ctrl = [mean_v{ctrl}];
 
-cellfun(@max, mean_v, 'UniformOutput', false)
+cellfun(@max, mean_v, 'UniformOutput', false) % Check if there are irregular values
