ed4b1840cf839624bdfbc8e8400ea6ed4f638f81
MoHeart666 <moheart666@outlook.com>
2026-09-02 15:02:53 +0800

    video_rotate_crop.m

--- video_rotate_crop.m
+++ video_rotate_crop.m
@@ -1,3 +1,5 @@
+%%Rotate a video then crop it
+
 Vpath = 'E:\Photometry_FP7_S5E2_Dlx\Home cage videos\20260106 FP7 home cage\Videos\CD1 1210 1208.mp4';
 input_v = VideoReader(Vpath);
 nFrames = input_v.NumFrames;
@@ -8,9 +10,9 @@ open(output_v2)
 for j = 1:nFrames
     try
         vframe = read(input_v, j);
-        vframe = imrotate(vframe,-45);
-        vframe_1 = vframe(171:620,241:490,1:3);
-        vframe_2 = vframe(361:800,481:750,1:3);
+        vframe = imrotate(vframe,-45); % set angle
+        vframe_1 = vframe(171:620,241:490,1:3); % crop
+        vframe_2 = vframe(361:800,481:750,1:3); % crop
         writeVideo(output_v1, vframe_1)
         writeVideo(output_v2, vframe_2)
     catch
