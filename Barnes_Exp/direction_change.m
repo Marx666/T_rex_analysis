ea07a20c34776ecbd45d89ddca9f2ba6d2da60e6
MoHeart666 <moheart666@outlook.com>
2026-09-02 15:44:35 +0800

    direction_change.m

--- Barnes_Exp/direction_change.m
+++ Barnes_Exp/direction_change.m
@@ -1,28 +1,19 @@
-% clear
-% load("xy.mat")
-% load("ishole.mat")
-% load("target.mat")
+%% Calculate the angle change of the animal body vector between two frames 
+
 anglec = cell(1,length(B));
 Vec = cell(1,length(B));
-% for i = 1:length(B)
-%     Vec{i} = [B{i}(:,5:6)-B{i}(:,3:4),zeros(length(B{i}),1)];
-% end
 
 for i = 1:length(B)
     for j = 2:length(B{i})
-        Vec{i}(j,:) = [B{i}(j,5:6)-B{i}(j-1,5:6),0];
+        Vec{i}(j,:) = [B{i}(j,5:6)-B{i}(j-1,5:6),0]; % define the body vector
     end
 end
 
-p = [0 0 1];
+p = [0 0 1]; % Normal vector
 for i = 1:length(Vec)
     for j = 3:length(Vec{i})
-        anglec{i}(j) = vecangle360(Vec{i}(j,:),Vec{i}(j-1,:),p);
+        anglec{i}(j) = vecangle360(Vec{i}(j,:),Vec{i}(j-1,:),p); % angle change
     end
-    % figure
-    % scatter3(B{i}(sum(ishole{i},2)==0,5),B{i}(sum(ishole{i},2)==0,6),anglec{i}(sum(ishole{i},2)==0),'Color','b')
-    % hold on
-    % scatter3(B{i}(sum(ishole{i},2)==1,5),B{i}(sum(ishole{i},2)==1,6),anglec{i}(sum(ishole{i},2)==1),'Color','r')
 end
 
 save('angle_change','anglec')
